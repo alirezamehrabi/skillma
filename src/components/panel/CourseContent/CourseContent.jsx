262557ff093125fc4f6e3caca7b08e84e9deeaf0
mehrabia95 <mehrabia95@yahoo.com>
2022-12-13 20:19:58 +0330

    cn

--- src/components/panel/CourseContent/CourseContent.jsx
+++ src/components/panel/CourseContent/CourseContent.jsx
@@ -12,7 +12,7 @@ const ContactSchema = Yup.object().shape({
   level: Yup.string().min(4, "Too Short!").required("Required"),
 });
 
-const CourseIntended = ({completeFormStep}) => {
+const CourseIntended = ({ completeFormStep }) => {
   const [f1, setF1] = useState("");
   const [f2, setF2] = useState("");
 
@@ -45,6 +45,56 @@ const CourseIntended = ({completeFormStep}) => {
         data-name="Path 83054"
         d="M22.858,12.267H14.915V4.324a1.324,1.324,0,0,0-2.648,0v7.943H4.324a1.324,1.324,0,0,0,0,2.648h7.943v7.943a1.324,1.324,0,0,0,2.648,0V14.915h7.943a1.324,1.324,0,0,0,0-2.648Z"
         transform="translate(0.53 0.53)"
+        fill="#7a7d7c"
+      />
+    </svg>
+  );
+  const del = (
+    <svg
+      id="Component_28_48"
+      data-name="Component 28 – 48"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <rect
+        id="Rectangle_6197"
+        data-name="Rectangle 6197"
+        width="24"
+        height="24"
+        fill="#7a7d7c"
+        opacity="0"
+      />
+      <path
+        id="Path_82778"
+        data-name="Path 82778"
+        d="M19.2,6.6H15.6V5.7A2.7,2.7,0,0,0,12.9,3H11.1A2.7,2.7,0,0,0,8.4,5.7v.9H4.8a.9.9,0,1,0,0,1.8h.9v9.9A2.7,2.7,0,0,0,8.4,21h7.2a2.7,2.7,0,0,0,2.7-2.7V8.4h.9a.9.9,0,0,0,0-1.8Zm-9-.9a.9.9,0,0,1,.9-.9h1.8a.9.9,0,0,1,.9.9v.9H10.2Zm6.3,12.6a.9.9,0,0,1-.9.9H8.4a.9.9,0,0,1-.9-.9V8.4h9Z"
+        fill="#7a7d7c"
+      />
+    </svg>
+  );
+  const edit = (
+    <svg
+      id="Component_29_43"
+      data-name="Component 29 – 43"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <rect
+        id="Rectangle_6196"
+        data-name="Rectangle 6196"
+        width="24"
+        height="24"
+        fill="#7a7d7c"
+        opacity="0"
+      />
+      <path
+        id="Path_82777"
+        data-name="Path 82777"
+        d="M20.092,12a.9.9,0,0,0-.9.9v5.4a.9.9,0,0,1-.9.9H5.7a.9.9,0,0,1-.9-.9V5.706a.9.9,0,0,1,.9-.9h5.4a.9.9,0,0,0,0-1.8H5.7A2.7,2.7,0,0,0,3,5.706V18.3A2.7,2.7,0,0,0,5.7,21H18.294a2.7,2.7,0,0,0,2.7-2.7V12.9A.9.9,0,0,0,20.092,12ZM6.6,12.686V16.5a.9.9,0,0,0,.9.9h3.814a.892.892,0,0,0,.638-.261L18.177,10.9h0l2.554-2.5a.9.9,0,0,0,0-1.272l0-.005L16.917,3.269a.9.9,0,0,0-1.272-.005l-.005.005L13.1,5.814h0L6.862,12.048A.892.892,0,0,0,6.6,12.686Zm9.678-7.51,2.545,2.545L17.547,9,15,6.453ZM8.4,13.055l5.333-5.334,2.546,2.546L10.945,15.6H8.4Z"
         fill="#7a7d7c"
       />
     </svg>
@@ -99,73 +149,36 @@ const CourseIntended = ({completeFormStep}) => {
             <div className={`col-12 ${co.navName}`}>Price & discount</div>
           </div>
         </div>
+      </div>
+      <div className={`row ${co.courseContent}`}>
+        <div className={`col-12 mb-5 mt-5`}>
+          
+            <h6 className={`${co.navtitle}`}>Introduction</h6>
+            <div className={` mb-4 ${co.icon}`}>
+              {del} {edit}
+            </div>
+            <div className={`row ${co.contentcourse}`}>
+          <div className={`col-3 d-flex justify-content-center ${co.newItem}`}>
+            <div className={`d-flex align-self-center`}>
+            {plus} Add New Lecture</div>
+            </div>
+          </div>
 
-        <Formik
-          initialValues={{
-            subject: "",
+        </div>
+        <div className={`col-12 mb-5 mt-5`}>
+          
+            <h6 className={`${co.navtitle}`}>season 1</h6>
+            <div className={` mb-4 ${co.icon}`}>
+              {del} {edit}
+            </div>
+            <div className={`row ${co.contentcourse}`}>
+          <div className={`col-3 d-flex justify-content-center ${co.newItem}`}>
+            <div className={`d-flex align-self-center`}>
+            {plus} Add New Lecture</div>
+            </div>
+          </div>
 
-            email: "",
-
-            message: "",
-          }}
-          validationSchema={ContactSchema}
-          onSubmit={(values) => {
-            // same shape as initial values
-
-            console.log(values);
-          }}
-        >
-          {({ errors, touched }) => (
-            <Form className={co.form}>
-              <h5 className={`fw-bold`}>
-                How many section does your course consist of?
-              </h5>
-              <label htmlFor="email" className={`${co.label} ${co.lbl1}`}>
-                Here is where you add course content, like lectures, course
-                sections, file and ...
-              </label>
-              <Field
-                name="email"
-                type="email"
-                placeholder="e.g. introduction"
-                className={`col-12 mx-auto ${co.txtfeild} ${co.txtfeild2}`}
-                value={f1}
-                onChange={handleNameChange}
-                maxLength="70"
-              />
-              <span className={`${co.txtlength} ${co.txtlength2}`}>
-                {70 - f1.length}
-              </span>
-              {errors.email && touched.email ? (
-                <div className={co.err}>{errors.email}</div>
-              ) : null}
-              <Field
-                name="email"
-                type="email"
-                placeholder="e.g. seasion 1"
-                className={`col-12 mx-auto ${co.txtfeild} ${co.txtfeild2}`}
-                value={f2}
-                onChange={handleNameChange2}
-                maxLength="70"
-              />
-              <span className={`${co.txtlength} ${co.txtlength2}`}>
-                {70 - f2.length}
-              </span>
-              {errors.email && touched.email ? (
-                <div className={co.err}>{errors.email}</div>
-              ) : null}
-
-              <div className={`col-12 mx-auto text center ${co.addBtn}`}>
-                {plus}Add more
-              </div>
-              <div className={`col-12 d-flex justify-content-end mt-4`}>
-                <button type="button" className={`${co.conBTN} ${co.nxt}`}>
-                  Continue
-                </button>
-              </div>
-            </Form>
-          )}
-        </Formik>
+        </div>
       </div>
     </SSRProvider>
   );
