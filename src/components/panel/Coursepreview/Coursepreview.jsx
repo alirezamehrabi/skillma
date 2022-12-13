3714de2fb8b0d110abadf417c0fd81200e8cf9c0
mehrabia95 <mehrabia95@yahoo.com>
2022-12-13 13:38:44 +0330

    x

--- src/components/panel/Coursepreview/Coursepreview.jsx
+++ src/components/panel/Coursepreview/Coursepreview.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useState,createRef } from "react";
 import { SSRProvider } from "react-bootstrap";
 import co from "../../../../styles/panel/course.module.css";
 import { Formik, Form, Field } from "formik";
@@ -20,6 +20,12 @@ const ContactSchema = Yup.object().shape({
 });
 
 const Courses = () => {
+  const [firstName, setFirstName] = useState('');
+
+  const handleNameChange = event => {
+    setFirstName(event.target.value);
+    console.log(event.target.value.length)
+  };
   return (
     <SSRProvider>
       <div className={`row ${co.preview}`}>
@@ -41,24 +47,28 @@ const Courses = () => {
         >
           {({ errors, touched }) => (
             <Form className={co.form}>
-              <label htmlFor="email">How about a working title?</label>
+              <label htmlFor="email" className={`${co.label}`}>How about a working title?</label>
               <Field
                 name="email"
                 type="email"
                 placeholder="e.g. Learn  ui/ux design"
-                className={`col-12 mx-auto ${co.field}`}
+                className={`col-12 mx-auto ${co.txtfeild}`}
+                value={firstName}
+                onChange={handleNameChange}
+                maxLength="160"
               />
-
+              <span className={`${co.txtlength}`}>{160- firstName.length}</span>
               {errors.email && touched.email ? (
                 <div className={co.err}>{errors.email}</div>
               ) : null}
-              <label htmlFor="subject">What category best fits the knowledge you'll share?</label>
+              <label htmlFor="subject" className={`${co.label}`}>What category best fits the knowledge you'll share?</label>
               <Field
                 as="select"
                 name="subject"
                 placeholder="Select category"
-                className={`col-12 mx-auto ${co.field}`}
+                className={`col-12 mx-auto ${co.txtfeild} ${co.selectFeild}`}
               >
+              <option value="" disabled defaultValue hidden>Select category</option>
               <option value="red">1</option>
               <option value="green">1</option>
               <option value="blue">1</option>
@@ -66,13 +76,14 @@ const Courses = () => {
               {errors.subject && touched.subject ? (
                 <div className={co.err}>{errors.subject}</div>
               ) : null}
-              <label htmlFor="subject">select level</label>
+              <label htmlFor="subject" className={`${co.label}`}>select level</label>
               <Field
                 as="select"
                 name="message"
                 placeholder="select level"
-                className={`col-12 mx-auto ${co.field}`}
+                className={`col-12 mx-auto ${co.txtfeild} ${co.selectFeild}`}
               >
+              <option value="" disabled defaultValue hidden>select level</option>
               <option value="red">1</option>
               <option value="green">1</option>
               <option value="blue">1</option>
@@ -80,14 +91,16 @@ const Courses = () => {
               {errors.message && touched.message ? (
                 <div className={co.err}>{errors.message}</div>
               ) : null}
-              <input type="checkbox" id="a"/>
-              <label htmlFor="a">This Course is Free</label>
-              <button type="button" className={co.conBTN}>
+              <input type="checkbox" id="a" className={`${co.chkbox}`}/>
+              <label htmlFor="a" className={`${co.label} ${co.chkboxlbl}`}>This Course is Free</label>
+              <div className={`col-12 d-flex justify-content-end mt-4`}>
+              <button type="button" className={`${co.conBTN} ${co.prev}`}>
               Previuse
               </button>
-              <button type="button" className={co.conBTN}>
+              <button type="button" className={`${co.conBTN} ${co.nxt}`}>
                 Contiue
               </button>
+              </div>
             </Form>
           )}
         </Formik>
